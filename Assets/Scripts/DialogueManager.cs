56fc26499e04b6506b6b89c4699b3f5e938c0953
Hamish <Hamish@pageland.co.nz>
2022-01-22 20:35:28 +1300

    Proper merge without loosing links
    This reverts commit af49c071392b82d23feaeb619030bc84496a8104.

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -20,6 +20,11 @@ public class DialogueManager : MonoBehaviour
 
     public bool testingString = false;
 
+    public GameObject basicDialogueParent;
+    public GameObject endDialogueParent;
+
+    public TextMeshProUGUI endingDisplay;
+
     private void Awake()
     {
         uiManager = GetComponent<UIManager>();
@@ -34,7 +39,14 @@ public class DialogueManager : MonoBehaviour
 
 
         currentDialogue = newDialogue;
+
         currentChoices = currentDialogue.Choices;
+
+        if (newDialogue.name.Contains("Ending"))
+        {
+            DisplayEndDialogue();
+        }
+
         //DebugCurrentDialogue();
         DisplayDialogue();
     }
@@ -60,11 +72,7 @@ public class DialogueManager : MonoBehaviour
 
     void DisplayDialogue()
     {
-#if UNITY_EDITOR
         dialogueName.text = currentDialogue.name;
-#else
-    dialogueName.Text = "";
-#endif
 
         uiManager.NewString(currentDialogue.Text);
         GetComponent<ButtonManager>().CreateButtons(currentDialogue.Choices, currentDialogue.DialogueType == DS.Enumerations.DSDialogueType.MultipleChoice);
@@ -73,5 +81,40 @@ public class DialogueManager : MonoBehaviour
     public void SelectChoice(int choice)
     {
         SetDialogue(currentChoices[choice].NextDialogue);
+    }
+
+    public void ResetDialogue()
+    {
+        endDialogueParent.SetActive(false);
+        basicDialogueParent.SetActive(true);
+
+        SetDialogue(container.UngroupedDialogues[0]);
+    }
+
+    public void DisplayEndDialogue()
+    {
+        endingDisplay.text = ToProperCase(currentDialogue.name.Replace("Ending", ""));
+
+        endDialogueParent.SetActive(true);
+        basicDialogueParent.SetActive(false);
+    }
+
+    string ToProperCase(string the_string)
+    {
+        // If there are 0 or 1 characters, just return the string.
+        if (the_string == null) return the_string;
+        if (the_string.Length < 2) return the_string.ToUpper();
+
+        // Start with the first character.
+        string result = the_string.Substring(0, 1).ToUpper();
+
+        // Add the remaining characters.
+        for (int i = 1; i < the_string.Length; i++)
+        {
+            if (char.IsUpper(the_string[i])) result += " ";
+            result += the_string[i];
+        }
+
+        return result;
     }
 }
